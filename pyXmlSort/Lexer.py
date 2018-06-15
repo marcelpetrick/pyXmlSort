f4931ac91cd3a9b694434ea136293b7d6543875f
Marcel Petrick <mail@marcelpetrick.it>
2018-06-15 17:29:09 +0200

    splitting the file-content into a list of string-tokens works

--- pyXmlSort/Lexer.py
+++ pyXmlSort/Lexer.py
@@ -1,7 +1,7 @@
 # Lexer.py
 
 from pyXmlSort.Token import Token
-#import String
+import re # for the regexp-splitting
 
 class Lexer:
     ''' task: read the file, remove all whitespace, separate into tokens with identified type '''
@@ -36,6 +36,7 @@ class Lexer:
                 line = line.replace("\n", "")
                 line = line.replace("\r", "")
                 line = line.replace("\t", "")
+                # todo replace with lstrip, rstrip
 
                 # add it to the complete string
                 #print(line)
@@ -51,8 +52,50 @@ class Lexer:
 
     def tokenizeString(self, inputString):
         ''' Create a list of tokens with the fitting content '''
-
-        returnValue = [Token(), Token()]
-
         print("** tokenizeString **")
+
+        returnValue = [Token(), Token()] # todo fix this
+
+        splitString = inputString.split(">")
+        stringTokenList = []
+        # splitting like this removed also the closing >: so add it back to each element
+        for elem in splitString:
+            # just handle non-empty strings
+            if elem != "":
+                fullItem = elem + ">"
+                # in case there is something like "true</CI>" then it has to be split too, before appending
+                if fullItem.__contains__("<"):
+                    posOpenBrace = fullItem.index("<")
+                    print("posOpenBrace:", posOpenBrace, fullItem)
+                    # todo continue
+                    if posOpenBrace != 0:
+                        beginning = fullItem[:posOpenBrace]
+                        ending = fullItem[posOpenBrace:]
+                        stringTokenList.append(beginning)
+                        stringTokenList.append(ending)
+                    else:
+                        stringTokenList.append(fullItem)
+                else:
+                    stringTokenList.append(fullItem)
+
+        #splitString = re.split("(>)", inputString)
+        print(splitString)
+        for elem in stringTokenList:
+            print(elem) # just for checking
+
         return returnValue
+
+# --------------------------------------------------------------------------------------------------------------------
+    # todo should be static ..
+    def cutToken(self, input):
+        ''' Returns token string and remaining string.'''
+
+        openBraceIndex = input.index("<")
+        closeBraceIndex = input.index(">")
+
+        # todo throw token-exception in case of openIndex >= closeIndex
+
+        if closeBraceIndex > openBraceIndex:
+            print("found correct indexes", openBraceIndex, closeBraceIndex)
+
+
